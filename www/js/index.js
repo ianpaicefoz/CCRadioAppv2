6989f120adc4f9407f6833b1bb2393d2b12e242d
IanPaice <ianpaicefoz@users.noreply.github.com>
2020-03-30 22:22:42 +0200

    admob

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,44 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var bannerAdUnit = "ca-app-pub-3096329003114803/7523999379";
+var interstitialAdUnit = "ca-app-pub-4906074177432504/1649035673";
+var isOverlap = true; //true: overlap, false: split
+var isTest = false;
+
+document.addEventListener("deviceready", function(){
+	window.admob.setUp(bannerAdUnit, interstitialAdUnit, isOverlap, isTest);
+	//
+	window.admob.onBannerAdPreloaded = function() {
+		/*alert('onBannerAdPreloaded');*/
+	};
+	window.admob.onBannerAdLoaded = function() {
+		/*alert('onBannerAdLoaded');*/
+	};
+	window.admob.onBannerAdShown = function() {
+		/*alert('onBannerAdShown');*/
+	};
+	window.admob.onBannerAdHidden = function() {
+		/*alert('onBannerAdHidden');*/
+	};	
+	//
+	window.admob.onInterstitialAdPreloaded = function() {
+		alert('onInterstitialAdPreloaded');
+	};
+	window.admob.onInterstitialAdLoaded = function() {
+		alert('onInterstitialAdLoaded');
+	};
+	window.admob.onInterstitialAdShown = function() {
+		alert('onInterstitialAdShown');
+	};
+	window.admob.onInterstitialAdHidden = function() {
+		alert('onInterstitialAdHidden');
+	};
+	
+	window.admob.showBannerAd('bottom-center', 'SMART_BANNER');
+	
+}, false);
+
 var app = {
     // Application Constructor
     initialize: function() {
